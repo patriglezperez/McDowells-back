0d3ad1e50b6d486d6f56ac671f229405dfa71da1
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-04-26 16:32:09 +0200

    fix execution flow

--- controllers/staff/postLogin.js
+++ controllers/staff/postLogin.js
@@ -8,16 +8,16 @@ async function postLogin(req, res) {
         /// if it is not a valid url we cancel???
         if (!validator.validate(loginEmail)) {
             res.status(400).json("Invalid Url"); 
-        }
-        /// We validate that it is encrypted???
-        const loginRetun = await staff.login(loginEmail, passWord);
-        /// cambiar estado
-        /// verificar rol para activar cola de cocina o reparto
-        if (loginRetun) {
-            res.json({"login": loginRetun[0].uuid_staff});
         } else {
-            res.status(404).json("Not found");
-        }
+            /// We validate that it is encrypted???
+            const loginRetun = await staff.login(loginEmail, passWord);
+            /// cambiar estado
+            /// verificar rol para activar cola de cocina o reparto
+            if (loginRetun) {
+                res.json({"login": loginRetun[0].uuid_staff});
+            } else {
+                res.status(404).json("Not found");
+        }}
     } catch (err) {
         res.status(500).json("Server Error");
     }
