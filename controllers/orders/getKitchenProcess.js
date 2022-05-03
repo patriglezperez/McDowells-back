8df0dcbd8ee2f174932dca4f6e5bf6b4cf0d593c
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-05-03 21:01:19 +0200

    awaiting adjustments by manager

--- controllers/orders/getKitchenProcess.js
+++ controllers/orders/getKitchenProcess.js
@@ -1,19 +1,22 @@
 const ordersManager = require("../../managers/ordersManager");
 const menuManager = require("../../managers/menuManager");
 
-async function checkExecutionTime(info) {
-    let timeNow = new Date();
-    // calacular tiempo de procesamiento del menu
 
-    if (info.date.getTime() < (timeNow + TIEMPO_DE_PROCESAMIENTO)) {
-        // cambiar a reparto camareros el menu
+
+async function checkExecutionTime(info, orders) {   
+    const timeNow = new Date().getTime();
+
+    // if finished we update the menu
+    if (info.chef[1] > timeNow) {
+        await orders.changeMenuSituation(info.uuid_menu, "delivering"); /// verificar campo
         return true
     } else {
         return false
     }
 }
 
-async function inKitchen(idCook) {
+async function inKitchen(idCook, orders) {
+
     let menuStatus = {"busy": false, "data": ""}
     const dateDayNow = (new Date()).toISOString().split("T")[0]; // YYYY-MM-DD now
 
@@ -22,7 +25,7 @@ async function inKitchen(idCook) {
     if (inKitchenArray) {       
         for (i=0; inKitchenArray.length > i; i++) {
             // we verify if the menu is finished its preparation
-            const finish = await checkExecutionTime(inKitchenArray[i])
+            const finish = await checkExecutionTime(inKitchenArray[i], orders)
             // we check if the cook is busy or not
             if ((inKitchenArray[i].chef[0] === idCook) & !finish) {
                 menuStatus.busy = true;
@@ -33,17 +36,31 @@ async function inKitchen(idCook) {
     return menuStatus;
 }
 
-async function getKitchenProcess(req, res) {
-    
-    const menuStatus = inKitchen(idCook);
+async function assignKitchenMenu(idCook, orders) {
+    /// proceso recuperar menu en process
+    const info = await orders.selectInOnlyOne("processing"); /// poner el puto nombre q quieras
+    /// proceso menus para pillar tiempo de duracion
+    const menu = new menuManager;
+    const processingTime = await menu.pickTime();
+    const timeNow = new Date().getTime();
+    /// añadir time
+    const completionTime = timeNow.setMinutes(timeNow.getMinutes() + processingTime);
+    const cook = [idCook, completionTime];
+    /// actualizar menu a cocina, añadimos en chef [uuid_chef, tiempo_q_estara_listo]
+    const deliveredDay = await orders.forKitchen(info.uuid_menu, "kitchen", cook); /// verificar campo
+    return deliveredDay;
+}
 
+async function getKitchenProcess(req, res) {
+    const orders = new ordersManager;
+    const idCook = 'params or body'; /// revisar
+    const menuStatus = inKitchen(idCook, orders);
+    let deliveredDay;
     if (!menuStatus.busy) {
-        assignKitchenMenu();
+        deliveredDay = await assignKitchenMenu(idCook, orders); /// ajustar
     }
     
     try {
-        const orders = new ordersManager; /// ver donde encaja mejor 
-        
         if (deliveredDay) {
             res.json({"deliveredDay": deliveredDay});
         } else {
