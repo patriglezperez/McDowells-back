c32e1d50e780db1fb9ff6ef17ba5831168cb9609
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-05-04 16:56:56 +0200

    status waiter

--- controllers/orders/getDelivering.js
+++ controllers/orders/getDelivering.js
@@ -9,6 +9,8 @@ const ordersManager = require("../../managers/ordersManager");
  */
  async function checkWaiter(idWaiter) {
     const staff = new staffManager;
+    /// si solo chekeamos por id de staff sacar la funcion de aqui meterlo en servicio
+    /// para usarlo en getKitchenProcess y no tener codigo duplicado
     const existsWaiter = await staff.checkFuckCook(idWaiter);
     if (existsWaiter) {
         return true
@@ -17,16 +19,48 @@ const ordersManager = require("../../managers/ordersManager");
     }
 }
 
+/**
+ * we check the status and update the orders in the Delivering
+ * @param {string} idWaiter uuid Waiter
+ * @param {object} orders connection object for order table
+ * @returns {object} waiterStatus
+ */
+async function inDelivering(idWaiter, orders) {
+    let waiterStatus = {"busy": false, "data": ""}
+    const dateDayNow = (new Date()).toISOString().split("T")[0]; // YYYY-MM-DD now
+
+    // we verify the orders in the delivering only those of the day
+    const inDeliveringArray = await orders.getByDateByStatus(dateDayNow, "delivering", idWaiter);
+    if (inDeliveringArray) {       
+        waiterStatus.busy = true;
+        waiterStatus.data = inDeliveringArray;
+    }
+    return waiterStatus;
+}
+
+/**
+ * We process the updating of Deliver processes
+ * /// es json o objeto ??? cual es la correcto ???
+ * @param {json} req
+ * @returns {json} res
+ */
 async function getDelivering(req, res) {
     try {
         let deliveredDay;
         // we retrieve the uuid associated with the waiter and his availability status
         const { waiter, status } = req.body;
         // we check if the waiter exists
-        const existsWaiter = await checkWaiter(waiter);
+        const existsWaiter = await checkWaiter(waiter); 
 
         if (existsWaiter) {
             const orders = new ordersManager;
+
+            // we check the menus in the delivered to see if our waiter is busy
+            const waiterStatus = inDelivering(waiter, orders);
+
+            if (!waiterStatus.busy && (status === "available")) {
+                deliveredDay = await assignDelivering(waiter, orders); /// ajustar ***
+            } 
 
         }
 
