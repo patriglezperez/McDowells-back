7639f0aadcbad55f59f938afa811593bd76b6cc5
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-05-04 14:26:03 +0200

    check waiter

--- controllers/orders/getDelivering.js
+++ controllers/orders/getDelivering.js
@@ -1,16 +1,36 @@
 const ordersManager = require("../../managers/ordersManager");
 
-async function getDelivering(req, res) {
-    const orders = new ordersManager;
-    const idCook = 'params or body'; /// revisar
-    const menuStatus = inKitchen(idCook, orders);
-    let deliveredDay;
-    if (!menuStatus.busy) {
-        deliveredDay = await assignKitchenMenu(idCook, orders); /// ajustar
-    }
 
+
+/**
+ * we verify if the Waiter exists or not
+ * @param {string} idWaiter uuid Waiter
+ * @returns {boolean} 
+ */
+ async function checkWaiter(idWaiter) {
+    const staff = new staffManager;
+    const existsWaiter = await staff.checkFuckCook(idWaiter);
+    if (existsWaiter) {
+        return true
+    } else {
+        return false
+    }
+}
+
+async function getDelivering(req, res) {
     try {
-        if (deliveredDay) {
+        let deliveredDay;
+        // we retrieve the uuid associated with the waiter and his availability status
+        const { waiter, status } = req.body;
+        // we check if the waiter exists
+        const existsWaiter = await checkWaiter(waiter);
+
+        if (existsWaiter) {
+            const orders = new ordersManager;
+
+        }
+
+        if (deliveredDay && existsWaiter) {
             res.json({"deliveredDay": deliveredDay});
         } else {
             res.status(404).json("Not found");
